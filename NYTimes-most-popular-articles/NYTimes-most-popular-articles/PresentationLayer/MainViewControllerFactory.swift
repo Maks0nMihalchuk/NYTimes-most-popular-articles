909d865174b764d2453f6edf768ac14a806a6ced
Maksym Mykhalchuk <maks.mihalchuk.2000@gmail.com>
2023-02-10 00:51:04 +0200

    moved the implementation of the MainViewControllerFactory protocol from DependencyProvider.swift to MainViewControllerFactory.swift. Added methods to set UITabBarItem and title in MainViewControllerFactory

--- NYTimes-most-popular-articles/NYTimes-most-popular-articles/PresentationLayer/MainViewControllerFactory.swift
+++ NYTimes-most-popular-articles/NYTimes-most-popular-articles/PresentationLayer/MainViewControllerFactory.swift
@@ -20,3 +20,78 @@ protocol MainViewControllerFactory {
     
     func buildArticleDetailsVC(services: Services) -> ArticleDetailsViewController
 }
+
+// MARK: - MainViewControllerFactory
+extension DependencyProvider: MainViewControllerFactory {
+    
+    func buildTabBarController() -> UITabBarController {
+        return UITabBarController()
+    }
+    
+    func buildMostEmailedVC(service: Services, routes: MPArticlesPresenterRoutes) -> MPArticlesViewController {
+        let viewController = MPArticlesViewController()
+        viewController.title = self.getScreenTitle(by: .mostEmailed)
+        viewController.tabBarItem = self.getTabBarItem(by: .mostEmailed)
+        let model = MostEmailedModel(articleProvider: service.articleProvider)
+        let presenter = MPArticlesPresenter(viewController, model: model, routes: routes)
+        viewController.presenter = presenter
+        viewController.tableViewDataSource = MPTableViewDataSource(presenter: presenter)
+        
+        return viewController
+    }
+    
+    func buildMostSharedVC(service: Services, routes: MPArticlesPresenterRoutes) -> MPArticlesViewController {
+        let viewController = MPArticlesViewController()
+        viewController.title = self.getScreenTitle(by: .mostShared)
+        viewController.tabBarItem = self.getTabBarItem(by: .mostShared)
+        let model = MostSharedModel(articleProvider: service.articleProvider)
+        let presenter = MPArticlesPresenter(viewController, model: model, routes: routes)
+        viewController.presenter = presenter
+        viewController.tableViewDataSource = MPTableViewDataSource(presenter: presenter)
+        
+        return viewController
+    }
+    
+    func buildMostViewedVC(service: Services, routes: MPArticlesPresenterRoutes) -> MPArticlesViewController {
+        let viewController = MPArticlesViewController()
+        viewController.title = self.getScreenTitle(by: .mostViewed)
+        viewController.tabBarItem = self.getTabBarItem(by: .mostViewed)
+        let model = MostViewedModel(articleProvider: service.articleProvider)
+        let presenter = MPArticlesPresenter(viewController, model: model, routes: routes)
+        viewController.presenter = presenter
+        viewController.tableViewDataSource = MPTableViewDataSource(presenter: presenter)
+        
+        return viewController
+    }
+    
+    func buildFavoritesArticlesVC(service: Services) -> FavoritesArticlesViewController {
+        let viewController = FavoritesArticlesViewController()
+        viewController.title = self.getScreenTitle(by: .favorites)
+        viewController.tabBarItem = self.getTabBarItem(by: .favorites)
+        let model = FavoritesArticlesModel()
+        let presenter = FavoritesArticlesPresenter(viewController, model: model)
+        viewController.presenter = presenter
+        
+        return viewController
+    }
+    
+    func buildArticleDetailsVC(services: Services) -> ArticleDetailsViewController {
+        let viewController = ArticleDetailsViewController()
+        let model = ArticleDetailsModel()
+        let presenter = ArticleDetailsPresenter(viewController, model: model)
+        viewController.presenter = presenter
+        return viewController
+    }
+    
+    // MARK: - setup components for screens 
+    private func getTabBarItem(by model: ScreenModel) -> UITabBarItem {
+        return UITabBarItem(title: model.title,
+                            image: UIImage(systemName: model.imageName),
+                            selectedImage: UIImage(systemName: model.selectedImageName))
+    }
+    
+    func getScreenTitle(by model: ScreenModel) -> String {
+        return model.title.uppercased()
+    }
+}
+
