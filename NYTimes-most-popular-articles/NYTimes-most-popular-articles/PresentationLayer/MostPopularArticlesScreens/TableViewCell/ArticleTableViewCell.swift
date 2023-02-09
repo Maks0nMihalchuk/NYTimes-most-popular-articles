47f0124565f230ecd73250b014a748d4af7d383e
Maksym Mykhalchuk <maks.mihalchuk.2000@gmail.com>
2023-02-09 22:38:42 +0200

    added information about the author of the article to display in the cell

--- NYTimes-most-popular-articles/NYTimes-most-popular-articles/PresentationLayer/MostPopularArticlesScreens/TableViewCell/ArticleTableViewCell.swift
+++ NYTimes-most-popular-articles/NYTimes-most-popular-articles/PresentationLayer/MostPopularArticlesScreens/TableViewCell/ArticleTableViewCell.swift
@@ -11,9 +11,11 @@ class ArticleTableViewCell: UITableViewCell {
     
     @IBOutlet private weak var articleTitleLabel: UILabel!
     
+    @IBOutlet private weak var articleAuthorLabel: UILabel!
     // MARK: - Public method for configure cell
     
-    func configureCell(with articleTitle: String) {
+    func configureCell(with articleTitle: String, author: String) {
         articleTitleLabel.text = articleTitle
+        articleAuthorLabel.text = "Author(s): \(author)"
     }
 }
